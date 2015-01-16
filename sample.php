17ec5444d7cc18af6f6e32234a443bb88d705ad7
zachriaul <zachriaul@gmail.com>
2015-01-16 10:13:42 -0400

    Clean up sample form

--- sample.php
+++ sample.php
@@ -38,7 +38,7 @@ include('header.php');
             <div class="form-group">
                 <label for="trailer" class="control-label col-xs-2">Trailer #</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" id="trailer" placeholder="">
+                    <input type="text" class="form-control" name="trailer">
                 </div>
             </div>
 
@@ -53,31 +53,24 @@ include('header.php');
             </div>
 
             <div class="form-group">
-                <label for="grader" class="control-label col-xs-2">Grader #</label>
+                <label for="toteSample" class="control-label col-xs-2">Tote / Sample #</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" id="grader" placeholder="">
+                    <input type="text" class="form-control" name="toteSample">
                 </div>
             </div>
 
-            <div class="form-group">
-                <label for="toteSample" class="control-label col-xs-2">Tote/Sample #</label>
-                <div class="col-xs-10">
-                    <input type="text" class="form-control" id="toteSample" placeholder="">
-                </div>
-            </div>
-
-            <!-- Pre-populated by the system date -->
             <div class="form-group">
                 <label for="date" class="control-label col-xs-2">Date</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" id="date" placeholder="">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label for="time" class="control-label col-xs-2">Time</label>
-                <div class="col-xs-10">
-                    <input type="text" class="form-control" id="time" placeholder="">
+                    <div class="form-group row">
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="date" placeholder="MM-DD-YYYY">
+                        </div>
+                        <label for="time" class="control-label col-xs-2">Time</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="time">
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -86,98 +79,215 @@ include('header.php');
             <div class="form-group">
                 <label for="totalWeight" class="control-label col-xs-2">Total Sample Weight</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="totalWeight" placeholder="">
+                    <input type="text" class="form-control" name="totalWeight">
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="unusable" class="control-label col-xs-2">Unusable</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="unusable" placeholder="">
+                    <div class="form-group row">
+                        <label for="useableWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="useableWeight">
+                        </div>
+                        <label for="useablePercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="useablePercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="rot" class="control-label col-xs-2">Rot</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="rot" placeholder="">
+                    <div class="form-group row">
+                        <label for="rotWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="rotWeight">
+                        </div>
+                        <label for="rotPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="rotPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="internal" class="control-label col-xs-2">Internal</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="internal" placeholder="">
+                    <div class="form-group row">
+                        <label for="internalWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="internalWeight">
+                        </div>
+                        <label for="internalPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="internalPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="pitRot" class="control-label col-xs-2">Pit Rot</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="pitRot" placeholder="">
+                    <div class="form-group row">
+                        <label for="pitRotWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="pitRotWeight" placeholder="">
+                        </div>
+                        <label for="pitRotPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="pitRotPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="wireworm" class="control-label col-xs-2">Wireworm</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="wireworm" placeholder="">
+                    <div class="form-group row">
+                        <label for="wirewormWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="wirewormWeight" placeholder="">
+                        </div>
+                        <label for="wirewormPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="wirewormPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="jellyEnd" class="control-label col-xs-2">Jelly End</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="jellyEnd" placeholder="">
+                    <div class="form-group row">
+                        <label for="jellyEndWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="jellyEndWeight" placeholder="">
+                        </div>
+                        <label for="jellyEndPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="jellyEndPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="other" class="control-label col-xs-2">Other</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="other" placeholder="">
+                    <div class="form-group row">
+                        <label for="otherWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="otherWeight" placeholder="">
+                        </div>
+                        <label for="otherPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="otherPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="hollowHeart" class="control-label col-xs-2">Hollow Heart</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="hollowHeart" placeholder="">
+                    <div class="form-group row">
+                        <label for="hollowHeartWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="hollowHeartWeight" placeholder="">
+                        </div>
+                        <label for="hollowHeartPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="hollowHeartPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="scab" class="control-label col-xs-2">Scab</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="scab" placeholder="">
+                    <div class="form-group row">
+                        <label for="scabWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="scabWeight" placeholder="">
+                        </div>
+                        <label for="scabPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="scabPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="sunburn" class="control-label col-xs-2">Sunburn</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="sunburn" placeholder="">
+                    <div class="form-group row">
+                        <label for="sunburnWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="sunburnWeight" placeholder="">
+                        </div>
+                        <label for="sunburnPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="sunburnPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="mechBruise" class="control-label col-xs-2">Mech Bruise</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="sunburn" placeholder="">
+                    <div class="form-group row">
+                        <label for="mechBruiseWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="mechBruiseWeight" placeholder="">
+                        </div>
+                        <label for="mechBruisePercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="mechBruisePercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="smalls" class="control-label col-xs-2">Smalls</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="smalls" placeholder="">
+                    <div class="form-group row">
+                        <label for="smallsWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="smallsWeight" placeholder="">
+                        </div>
+                        <label for="smallsPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="smallsPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="10oz" class="control-label col-xs-2">10oz</label>
                 <div class="col-xs-10">
-                    <input type="text" class="form-control" name="10oz" placeholder="">
+                    <div class="form-group row">
+                        <label for="10ozWeight" class="col-xs-1 control-label">Weight</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="10ozsWeight" placeholder="">
+                        </div>
+                        <label for="10ozPercent" class="col-xs-1 control-label">Percent</label>
+                        <div class="col-xs-5">
+                            <input type="text" class="form-control" name="10ozPercent" placeholder="%" disabled>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -201,8 +311,6 @@ include('header.php');
                     <input type="text" class="form-control" name="rockMaterial" placeholder="">
                 </div>
             </div>
-
- 
 
             <div class="form-group">
                 <div class="col-xs-offset-2 col-xs-10">
