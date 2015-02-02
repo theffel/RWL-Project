9706f40e76f4f8ba38d47fda43fc1b89bfe92d7b
zachriaul <zachriaul@gmail.com>
2015-02-02 12:09:51 -0400

    Fix weird errors

--- pickup/pickup_script.php
+++ pickup/pickup_script.php
@@ -59,7 +59,6 @@ $result = $db->query($query);
 if (!empty($result)) {
 	while ($row = $result->fetch_assoc()){
 		$pickupId = $row['pickup_id'];
-<<<<<<< HEAD
 		$date = $row['pd_date'];
 		$truck = $row['truck_num']; 
 		$trailer = $row['trailer_num'];
@@ -92,7 +91,6 @@ if (!empty($result)) {
 			$_SESSION['editIncomingDeliveries'] = $editIncomingDeliveries;
 			header ("location:edit_pickup.php?id=" . $_SESSION['deliveryNum'] );
 		}
-=======
     	$date = $row['pd_date'];
     	$truck = $row['truck_num']; 
     	$trailer = $row['trailer_num'];
@@ -100,32 +98,6 @@ if (!empty($result)) {
     	$warehouse = $row['warehouse_name']; 
     	$incomingDeliveries[] = array($pickupId, $date, $truck, $trailer, $farm, $warehouse);
     	$_SESSION['incomingDeliveries'] = $incomingDeliveries;
-	}
-}
-
-// Select incoming deliveries 
-for ($x = 0; $x < count($_SESSION['incomingDeliveries']); $x++){	
-    if (isset($_POST[$incomingDeliveries[$x][0]])) {
-  		$_SESSION['deliveryNum'] = $incomingDeliveries[$x][0];
-  		$query = "SELECT arrive_time_farm, load_time, depart_time_farm, arrive_time_rwl, unload_time, depart_time_rwl, ticket_num, gross_weight, tare_weight 
-  			FROM pick_up WHERE pickup_id = " . $_SESSION['deliveryNum'];
-						var_dump($query);
-    	$result = $db->query($query);
-    	$row = $result->fetch_assoc();
-		$arriveTimeFarm = $row['arrive_time_farm'];
-	    $loadTime = $row['load_time'];
-	    $departTimeFarm = $row['depart_time_farm']; 
-	    $arriveTimeRwl = $row['arrive_time_rwl'];
-	    $unloadTime = $row['unload_time'];   
-	    $departTimeRwl = $row['depart_time_rwl']; 
-	    $ticketNum = $row['ticket_num'];   
-	    $grossWeight = $row['gross_weight']; 
-	    $tareWeight = $row['tare_weight'];   
-    	$editIncomingDeliveries[] = array($arriveTimeFarm, $loadTime, $departTimeFarm, $arriveTimeRwl, $unloadTime, $departTimeRwl, $ticketNum,
-    		$grossWeight, $tareWeight); 
-    	$_SESSION['editIncomingDeliveries'] = $editIncomingDeliveries;
-        header("location:edit_pickup.php?id=" . $_SESSION['deliveryNum'] );
->>>>>>> origin/master
 	}
 }
 
