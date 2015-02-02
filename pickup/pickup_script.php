0d70f6dc88dfd98a86f6b5bb44d6e6102a4ab6b0
zachriaul <zachriaul@gmail.com>
2015-02-02 11:49:01 -0400

    Fix pickup error

--- pickup/pickup_script.php
+++ pickup/pickup_script.php
@@ -56,15 +56,17 @@ $query = "SELECT pickup_id, pd_date, truck_num, trailer_num, farm_name, warehous
 			WHERE pd_date LIKE '" . $currentDate . "%' AND emp_id = " . $empId .  " ORDER BY pd_date DESC";
 $result = $db->query($query);
 
-while ($row = $result->fetch_assoc()){
-	$pickupId = $row['pickup_id'];
-    $date = $row['pd_date'];
-    $truck = $row['truck_num']; 
-    $trailer = $row['trailer_num'];
-    $farm = $row['farm_name'];   
-    $warehouse = $row['warehouse_name']; 
-    $incomingDeliveries[] = array($pickupId, $date, $truck, $trailer, $farm, $warehouse);
-    $_SESSION['incomingDeliveries'] = $incomingDeliveries;
+if (!empty($result)) {
+	while ($row = $result->fetch_assoc()){
+		$pickupId = $row['pickup_id'];
+    	$date = $row['pd_date'];
+    	$truck = $row['truck_num']; 
+    	$trailer = $row['trailer_num'];
+    	$farm = $row['farm_name'];   
+    	$warehouse = $row['warehouse_name']; 
+    	$incomingDeliveries[] = array($pickupId, $date, $truck, $trailer, $farm, $warehouse);
+    	$_SESSION['incomingDeliveries'] = $incomingDeliveries;
+	}
 }
 
 // Select incoming deliveries 
@@ -88,7 +90,7 @@ for ($x = 0; $x < count($_SESSION['incomingDeliveries']); $x++){
     	$editIncomingDeliveries[] = array($arriveTimeFarm, $loadTime, $departTimeFarm, $arriveTimeRwl, $unloadTime, $departTimeRwl, $ticketNum,
     		$grossWeight, $tareWeight); 
     	$_SESSION['editIncomingDeliveries'] = $editIncomingDeliveries;
-        header ("location:edit_pickup.php?id=" . $_SESSION['deliveryNum'] );
+        header("location:edit_pickup.php?id=" . $_SESSION['deliveryNum'] );
 	}
 }
 
@@ -112,6 +114,6 @@ if (isset($_POST['update'])) {
 	
 	// kill session var 'editIncomingDeliveries'
 	unset($_SESSION['editIncomingDeliveries']);
-	header ("location:index.php");
+	header("location:index.php");
 } 
 ?>
