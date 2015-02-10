bea136b71668aeae71f312602ad0c8536a8878f1
zachriaul <zachriaul@gmail.com>
2015-02-10 09:22:30 -0400

    Finish product reception

---
+++ product_reception/edit_product.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Description for file goes here.
+ *
+ * PHP version 5
+ *
+ *
+ * @category    CategoryName
+ * @package     PackageName
+ * @author      Zachary Theriault
+ * @copyright   2015 sCIS
+ * @license     http://php.net/license/3_01.txt  PHP License 3.01
+ * @version     1.00
+ * @link        http://pear.php.net/package/PackageName
+ * @since       2015-02-10
+ */
+
+// Start the session
+session_start();
+
+// Include the php files
+include('../database.php');
+include('../header.php');
+include('product_script.php');
+?>
+    <!-- Page Content -->
+    <div class="container">
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Product Reception</h1>
+                <ol class="breadcrumb">
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="index.php">Product Reception</a></li>
+                    <li class="active">Edit</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+        <?php
+        $loggedIn = (!empty($_SESSION['loggedIn'])) ? $_SESSION['loggedIn'] : "";
+        $employeeType = (!empty($_SESSION['employeeType'])) ? $_SESSION['employeeType'] : "";
+        $attendanceId = (!empty($_SESSION['attendanceId'])) ? $_SESSION['attendanceId'] : "";
+        // If the user is logged in with the correct employee permissions
+        if ($loggedIn == true && $attendanceId =! 0 && $employeeType == 1 || $employeeType == 3) {
+        ?>
+        <h2 class="page-header">Edit Product</h2>
+        <form class="form-horizontal" name="receptionForm" id="receptionForm" method="post" action="edit_product.php">
+            <div class="form-group">
+                <label for="date" class="control-label col-md-2">Date</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="date" value="<?php echo $_SESSION['editProductionReception'][0][0]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="farm" class="control-label col-md-2">Farm</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="farm" value="<?php echo $_SESSION['editProductionReception'][0][1]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="potato" class="control-label col-md-2">Potato</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="potato" value="<?php echo $_SESSION['editProductionReception'][0][2]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="loadIDInfo" class="control-label col-md-2">Load ID Info</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="loadIDInfo" value="<?php echo $_SESSION['editProductionReception'][0][3]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="quanRecieved" class="control-label col-md-2">Quantity Recieved</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="quanRecieved" value="<?php echo $_SESSION['editProductionReception'][0][4]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="bulkOther" class="control-label col-md-2">Bulk or Other</label>
+                <div class="col-md-10">
+                    <ul class="list-inline">
+                        <?php
+                        if ($_SESSION['editProductionReception'][0][5] == 0) {
+                            echo '<li><input type="radio" name="bulkOther" value="0" checked> Trailer</li>';
+                            echo '<li><input type="radio" name="bulkOther" value="1"> Tandom</li>';
+                            echo '<li><input type="radio" name="bulkOther" value="2"> Other</li>';
+                        } else if ($_SESSION['editProductionReception'][0][5] == 1) {
+                            echo '<li><input type="radio" name="bulkOther" value="0"> Trailer</li>';
+                            echo '<li><input type="radio" name="bulkOther" value="1" checked> Tandom</li>';
+                            echo '<li><input type="radio" name="bulkOther" value="2"> Other</li>';
+                        } else {
+                            echo '<li><input type="radio" name="bulkOther" value="0"> Trailer</li>';
+                            echo '<li><input type="radio" name="bulkOther" value="1"> Tandom</li>';
+                            echo '<li><input type="radio" name="bulkOther" value="2" checked> Other</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="washed" class="control-label col-md-2">Washed</label>
+                <div class="col-md-10">
+                    <ul class="list-inline">
+                        <?php
+                        if ($_SESSION['editProductionReception'][0][4] == 0) {
+                            echo '<li><input type="radio" name="washed" value="0" checked> Yes</li>';
+                            echo '<li><input type="radio" name="washed" value="1"> No</li>';
+                        } else {
+                            echo '<li><input type="radio" name="washed" value="0"> Yes</li>';
+                            echo '<li><input type="radio" name="washed" value="1" checked> No</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="cleanliness" class="control-label col-md-2">Cleanliness of Trailer After Unloading</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="quanRecieved" value="<?php echo $_SESSION['editProductionReception'][0][7]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="CFIANotified" class="control-label col-md-2">CFIA Notified</label>
+                <div class="col-md-10">
+                    <ul class="list-inline">
+                        <?php
+                        if ($_SESSION['editProductionReception'][0][8] == 0) {
+                            echo '<li><input type="radio" name="CFIANotified" value="0" checked> Yes</li>';
+                            echo '<li><input type="radio" name="CFIANotified" value="1"> No</li>';
+                        } else {
+                            echo '<li><input type="radio" name="CFIANotified" value="0"> Yes</li>';
+                            echo '<li><input type="radio" name="CFIANotified" value="1" checked> No</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="CFIANotifiedBy" class="control-label col-md-2">CFIA Notified By</label>
+                <div class="col-md-10">
+                    <input type="text" class="form-control" name="quanRecieved" value="<?php echo $_SESSION['editProductionReception'][0][9]; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="movementCert" class="control-label col-md-2">Movement Certifcate</label>
+                <div class="col-md-10">
+                    <ul class="list-inline">
+                        <?php
+                        if ($_SESSION['editProductionReception'][0][10] == 0) {
+                            echo '<li><input type="radio" name="movementCert" value="Yes" checked> Yes</li>';
+                            echo '<li><input type="radio" name="movementCert" value="No"> No</li>';
+                        } else {
+                            echo '<li><input type="radio" name="movementCert" value="Yes"> Yes</li>';
+                            echo '<li><input type="radio" name="movementCert" value="No"> No</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="accepted" class="control-label col-md-2">Accepted</label>
+                <div class="col-md-10">
+                    <ul class="list-inline">
+                        <?php
+                        if ($_SESSION['editProductionReception'][0][11] == 0) {
+                            echo '<li><input type="radio" name="accepted" value="Yes" checked> Yes</li>';
+                            echo '<li><input type="radio" name="accepted" value="No"> No</li>';
+                        } else {
+                            echo '<li><input type="radio" name="accepted" value="Yes"> Yes</li>';
+                            echo '<li><input type="radio" name="accepted" value="No"> No</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-offset-2 col-md-10">
+                    <input type="submit" class="btn btn-primary" name="update" value="Update"/>
+                </div>
+            </div>
+        </form>
+        <?php
+        } else {
+            echo "<h2>You do not have permission to view this page.</h2>";
+        }
+        // Include the footer.php file
+        include('../footer.php');
+        ?>
+    </div>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="../js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
--- product_reception/product_script.php
+++ product_reception/product_script.php
@@ -38,7 +38,7 @@ if (isset($_POST['submit'])) {
 	$result = $db->query($query);
 }
 
-// Load array with product receptions for day by employee
+// Load array with product reception for day by employee
 $query = "SELECT reception_id, reception_date, potato_name, farm_name, load_info_id, quantity_recieved FROM production_reception INNER JOIN potato ON production_reception.potato_id = potato.potato_id INNER JOIN farm ON production_reception.farm_id = farm.farm_id WHERE reception_date LIKE '" . $currentDate . "%' AND emp_id = " . $empId .  " ORDER BY reception_date DESC";
 $result = $db->query($query);
 
@@ -54,44 +54,62 @@ if (!empty($result)) {
     	$_SESSION['productionReception'] = $productionReception;
 	}
 
-	// Select fuel receipts
-	// for ($x = 0; $x < count($_SESSION['productionReception']); $x++) {
-	// 	if (isset($_POST[$productionReception[$x][0]])) {
-	// 		$_SESSION['receptionNum'] = $productionReception[$x][0];
-	// 		$query = "SELECT reception_date, potato_name, farm_name, load_info_id, quantity_recieved, washed, trailer_tandom, CFIA_notified, movement_certificate, accepted FROM production_reception INNER JOIN potato ON production_reception.potato_id = potato.potato_id INNER JOIN farm ON production_reception.farm_id = farm.farm_id WHERE reception_id = " . $_SESSION['receptionNum'];
-	// 		$result = $db->query($query);
-	// 		$row = $result->fetch_assoc();
-	// 		$date = $row['reception_date'];
- //    		$farm = $row['farm_name'];
- //    		$potato = $row['potato_name'];
- //    		$loadIDInfo = $row['load_info_id'];
- //    		$quantity = $row['quantity_recieved'];
-	// 		$editProductionReception[] = array($date, $truck, $mileage, $litres, $cost, $location); 
-	// 		$_SESSION['editProductionReception'] = $editReceipt;
-	// 		header("location:edit_production.php?id=" . $_SESSION['receptionNum'] );
-	// 	}
-	// }
+	// Select product reception
+	for ($x = 0; $x < count($_SESSION['productionReception']); $x++) {
+		if (isset($_POST[$productionReception[$x][0]])) {
+			$_SESSION['receptionNum'] = $productionReception[$x][0];
+			$query = "SELECT reception_date, potato_name, farm_name, load_info_id, quantity_recieved, washed, trailer_tandom, CFIA_notified, notified_by, movement_certificate, accepted, cleanliness FROM production_reception INNER JOIN potato ON production_reception.potato_id = potato.potato_id INNER JOIN farm ON production_reception.farm_id = farm.farm_id WHERE reception_id = " . $_SESSION['receptionNum'];
+			$result = $db->query($query);
+			$row = $result->fetch_assoc();
+			$date = $row['reception_date'];
+    		$farm = $row['farm_name'];
+    		$potato = $row['potato_name'];
+    		$loadIDInfo = $row['load_info_id'];
+    		$quantity = $row['quantity_recieved'];
+    		$bulkOther = $row['trailer_tandom'];
+    		$washed = $row['washed'];
+    		$cleanliness = $row['cleanliness'];
+    		$CFIANotified = $row['CFIA_notified'];
+    		$CFIANotifiedBy = $row['notified_by'];
+    		$movementCert = $row['movement_certificate'];
+    		$accepted = $row['accepted'];
+			$editProductionReception[] = array($date, $farm, $potato, $loadIDInfo, $quantity, $bulkOther, $washed, $cleanliness, $CFIANotified, $CFIANotifiedBy, $movementCert, $accepted); 
+			$_SESSION['editProductionReception'] = $editReceipt;
+			header("location:edit_product.php?id=" . $_SESSION['receptionNum'] );
+		}
+	}
 }
 
-// // Update fuel
-// if (isset($_POST['update'])) {	
-// 	$date = $db->real_escape_string($_POST['date']);	
-// 	$truck = $db->real_escape_string($_POST['truck']);
-// 	$mileage = $db->real_escape_string($_POST['mileage']);
-// 	$litres = $db->real_escape_string($_POST['litres']);
-// 	$cost = $db->real_escape_string($_POST['cost']);
-// 	$location = $db->real_escape_string($_POST['location']);
+// Update product reception
+if (isset($_POST['update'])) {	
+	$date = $db->real_escape_string($_POST['date']);	
+	$farm = $db->real_escape_string($_POST['farm']);
+	$potato = $db->real_escape_string($_POST['potato']);
+	$loadIDInfo = $db->real_escape_string($_POST['loadIDInfo']);
+	$quantity = $db->real_escape_string($_POST['quanRecieved']);
+	$bulkOther = $db->real_escape_string($_POST['bulkOther']);
+	$washed = $db->real_escape_string($_POST['washed']);
+	$cleanliness = $db->real_escape_string($_POST['cleanliness']);
+	$CFIANotified = $db->real_escape_string($_POST['CFIANotified']);
+	$CFIANotifiedBy = $db->real_escape_string($_POST['CFIANotifiedBy']);
+	$movementCert = $db->real_escape_string($_POST['movementCert']);
+	$accepted = $db->real_escape_string($_POST['accepted']);
 	
-// 	$query = "SELECT truck_id FROM truck WHERE truck_num = '" . $truck . "'";
-// 	$result = $db->query($query);
-// 	$row = $result->fetch_assoc();
-// 	$truckId = $row['truck_id'];
+	$query = "SELECT farm_id FROM farm WHERE farm_name = '" . $farm . "'";
+	$result = $db->query($query);
+	$row = $result->fetch_assoc();
+	$farmId = $row['farm_id'];
+
+	$query = "SELECT potato_id FROM potato WHERE potato_name = '" . $potato . "'";
+	$result = $db->query($query);
+	$row = $result->fetch_assoc();
+	$potatoId = $row['potato_id'];
 	
-// 	$query = "UPDATE fuel SET truck_id = " . $truckId . ", purchase_date = '" . $date . "', mileage =" . $mileage . ", litres = " . $litres . ", cost = " . $cost . ", location = '" . $location . "' WHERE fuel_id = " . $_SESSION['receiptNum'];
-// 	$result = $db->query($query);
+	$query = "UPDATE production_reception SET reception_date = '" . $date . "', potato_id = " . $potatoId . ", farm_id =" . $farmId . ", load_info_id = " . $loadIDInfo . ", quantity_recieved = " . $quanRecieved . ", washed = " . $washed . ", trailer_tandom = " . $bulkOther . ", CFIA_notified = " . $CFIANotified . ", notified_by = " . $CFIANotifiedBy . ", movement_certificate = " . $movementCert . ", accepted = " . $accepted . ", cleanliness = " . $cleanliness . " WHERE reception_id = " . $_SESSION['receptionNum'];
+	$result = $db->query($query);
 	
-// 	// kill session var 'fuelReceipts'
-// 	unset($_SESSION['fuelReceipts']);
-// 	header("location:index.php");
-// }
+	// kill session var 'productionReception'
+	unset($_SESSION['productionReception']);
+	header("location:index.php");
+}
 ?>
