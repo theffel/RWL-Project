537be703c748c11bb571ce2f0e3fbcd1c9b2ad8d
zachriaul <zachriaul@gmail.com>
2015-02-11 10:11:34 -0400

    Rejection

--- rejection/edit_rejection.php
+++ rejection/edit_rejection.php
@@ -12,7 +12,7 @@
  * @license     http://php.net/license/3_01.txt  PHP License 3.01
  * @version     1.00
  * @link        http://pear.php.net/package/PackageName
- * @since       2015-01-15
+ * @since       2015-02-11
  */
 
 // Start the session
@@ -30,7 +30,8 @@ include('../header.php');
                 <h1 class="page-header">Rejection</h1>
                 <ol class="breadcrumb">
                     <li><a href="../index.php">Home</a></li>
-                    <li class="active">Rejection</li>
+                    <li><a href="index.php">Rejection</a></li>
+                    <li class="active">Edit</li>
                 </ol>
             </div>
         </div>
@@ -42,12 +43,12 @@ include('../header.php');
         // If the user is logged in with the correct employee permissions
         if ($loggedIn == true && $attendanceId =! 0 && $employeeType == 3 || $employeeType == 2) {
         ?>
-        <h2 class="page-header">Add a Rejected Load</h2>
-        <form class="form-horizontal" name="rejectForm" id="rejectForm" method="post" action="index.php">
+        <h2 class="page-header">Edit Rejected Load</h2>
+        <form class="form-horizontal" name="rejectForm" id="rejectForm" method="post" action="edit_rejection.php">
             <div class="form-group">
                 <label for="date" class="control-label col-md-2">Date</label>
                 <div class="col-md-10">
-                    <input type="text" class="form-control" name="date" value="<?php echo $dateTime; ?>">
+                    <input type="text" class="form-control" name="date" value="<?php echo $_SESSION['editRejection'][0][0]; ?>">
                 </div>
             </div>
             <div class="form-group">
@@ -56,7 +57,11 @@ include('../header.php');
                     <select class="form-control" name="potato">
                         <?php
                         for ($x = 0; $x < count($potatoes); $x++){
-                            echo '<option value="' . $potatoes[$x][0] .'">' . $potatoes[$x][1] .'</option>';
+                            if ($potatoes[$x][1] == $_SESSION['editRejection'][0][1]) {
+                                echo '<option selected value="' . $potatoes[$x][0] . '">' . $potatoes[$x][1] . '</option>';
+                            } else {
+                                echo '<option value="' . $potatoes[$x][0] . '">' . $potatoes[$x][1] . '</option>';
+                            }
                         }
                         ?>
                     </select>
@@ -68,7 +73,11 @@ include('../header.php');
                     <select class="form-control" name="farm">
                         <?php
                         for ($x = 0; $x < count($farms); $x++){
-                            echo '<option value="' . $farms[$x][0] .'">' . $farms[$x][1] .'</option>';
+                            if ($farms[$x][1] == $_SESSION['editRejection'][0][2]) {
+                                echo '<option selected value="' . $farms[$x][0] . '">' . $farms[$x][1] . '</option>';
+                            } else {
+                                echo '<option value="' . $farms[$x][0] . '">' . $farms[$x][1] . '</option>';
+                            }
                         }
                         ?>
                     </select>
@@ -77,21 +86,28 @@ include('../header.php');
             <div class="form-group">
                 <label for="ticketNumber" class="control-label col-md-2">Ticket Number</label>
                 <div class="col-md-10">
-                    <input type="text" class="form-control" name="ticketNumber">
+                    <input type="text" class="form-control" name="ticketNumber" value="<?php echo $_SESSION['editRejection'][0][3]; ?>">
                 </div>
             </div>
             <div class="form-group">
                 <label for="quantReturned" class="control-label col-md-2">Quantity Returned</label>
                 <div class="col-md-10">
-                    <input type="text" class="form-control" name="quantReturned">
+                    <input type="text" class="form-control" name="quantReturned" value="<?php echo $_SESSION['editRejection'][0][4]; ?>">
                 </div>
             </div>
             <div class="form-group">
                 <label for="rewashed" class="control-label col-md-2">Was Product Re-Washed</label>
                 <div class="col-md-10">
                     <ul class="list-inline">
-                        <li><input type="radio" name="rewashed" value="0"> Yes</li>
-                        <li><input type="radio" name="rewashed" value="1"> No</li>
+                        <?php
+                        if ($_SESSION['editRejection'][0][5] == 0 ) {
+                            echo '<li><input type="radio" name="rewashed" value="0" checked> Yes</li>';
+                            echo '<li><input type="radio" name="rewashed" value="1"> No</li>';
+                        } else {
+                            echo '<li><input type="radio" name="rewashed" value="0" > Yes</li>';
+                            echo '<li><input type="radio" name="rewashed" value="1" checked> No</li>';
+                        }
+                        ?>
                     </ul>
                 </div>
             </div>
@@ -99,8 +115,15 @@ include('../header.php');
                 <label for="regraded" class="control-label col-md-2">Was Product Re-Graded</label>
                 <div class="col-md-10">
                     <ul class="list-inline">
-                        <li><input type="radio" name="regraded" value="0"> Yes</li>
-                        <li><input type="radio" name="regraded" value="1"> No</li>
+                        <?php
+                        if ($_SESSION['editRejection'][0][6] == 0 ) {
+                            echo '<li><input type="radio" name="regraded" value="0" checked> Yes</li>';
+                            echo '<li><input type="radio" name="regraded" value="1"> No</li>';
+                        } else {
+                            echo '<li><input type="radio" name="regraded" value="0" > Yes</li>';
+                            echo '<li><input type="radio" name="regraded" value="1" checked> No</li>';
+                        }
+                        ?>
                     </ul>
                 </div>
             </div>
@@ -108,45 +131,28 @@ include('../header.php');
                 <label for="prodReturned" class="control-label col-md-2">Product Returned</label>
                 <div class="col-md-10">
                     <ul class="list-inline">
-                        <li><input type="checkbox" name="prodReturned" value="0"> Processor</input></li>
-                        <li><input type="checkbox" name="prodReturned" value="1"> Producer</input></li>
+                        <?php
+                        if($_SESSION['editRejection'][0][7] == 0){
+                            echo '<input type="checkbox" name="prodReturned" value="1" checked> Processor</input>';
+                        } else {
+                            echo '<input type="checkbox" name="prodReturned" value="0"> Processor</input>';
+                        }
+                        if($_SESSION['editRejection'][0][7] == 1){
+                            echo '<input type="checkbox" name="prodReturned" value="1" checked> Producer</input>';
+                        } else {
+                            echo '<input type="checkbox" name="prodReturned" value="0"> Producer</input>';
+                        }
+                        ?>
                     </ul>
                 </div>
             </div>
             <div class="form-group">
                 <div class="col-md-offset-2 col-md-10">
-                    <input type="submit" class="btn btn-primary" name="submit" value="Submit"/>
+                    <input type="submit" class="btn btn-primary" name="update" value="Update"/>
                 </div>
             </div>
-        <hr>
-        <h2 class="page-header">Edit Rejection</h2>
+        </form>
         <?php
-            if (!empty($rejections)) {
-                echo '<table class="table">
-                        <thead>
-                           <tr>
-                                <th>Date</th>
-                                <th>Potato</th>
-                                <th>Farm</th>
-                                <th>Quantity Returned</th>
-                                <th>Returned To</th>
-                            </tr>
-                        </thead>
-                        <tbody>';
-                for ($x = 0; $x < count($rejections); $x++) {
-                    echo '<tr>
-                        <td>'. $rejections[$x][1].'</td>
-                        <td>'. $rejections[$x][2].'</td>
-                        <td>'. $rejections[$x][3].'</td>
-                        <td>'. $rejections[$x][4].'</td>
-                        <td>'. $rejections[$x][5].'</td>
-                        <td><input type="submit" class="btn btn-primary" name="'. $rejections[$x][0].'" value="Edit"/></td>
-                    </tr>';
-                }
-                echo '</tbody></table></form>';
-            } else {
-                echo "<p>There are currently no rejections to view.</p>";
-            }
         } else {
             echo "<h2>You do not have permission to view this page.</h2>";
         }
